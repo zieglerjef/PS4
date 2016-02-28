b16c920578757cee3522c54ff4a39cbd529ab858
zieglerjef <zieglerjef@gmail.com>
2016-02-28 15:38:14 -0600

    Initial web scrape of wikipedia

--- PS4.R
+++ PS4.R
@@ -1,10 +1,23 @@
-library(rvest)    
+##########################
+### Scraping Wikipedia ###
+##########################
+
+# load libraries
+library(htmltab)
+
+# load wikipedia page with election table
 wikiURL <- 'https://en.wikipedia.org/wiki/List_of_United_States_presidential_elections_by_popular_vote_margin'
 
-## Grab the tables from the page and use the html_table function to extract the tables.
-## You need to subset temp to find the data you're interested in (HINT: html_table())
+# by using htmltab, it removes the span from the table, which would require much more code to clean
+electionResults <- htmltab(doc = wikiURL, which = "//th[text() = 'Year']/ancestor::table")
 
-temp <- wikiURL %>% 
-  read_html %>%
-  html_nodes("table")
+# rename columns of electionResults
+colnames(electionResults) <- c("Number","Year","Winner","Winner Party",
+                               "Winner Pop. vote (%)","Win Margin (%)","Winner Pop. vote (total)",
+                               "Win Margin (total)","Runner-up","Runner-up Party",
+                               "Turnout")
 
+# remove % sign from entries
+electionResults$"Winner Pop. vote (%)" <- as.numeric(gsub("%", "", as.character(electionResults$"Winner Pop. vote (%)")))
+electionResults$"Win Margin (%)" <- gsub("%", "", as.character(electionResults$"Win Margin (%)"))
+electionResults$Turnout <- as.numeric(gsub("%", "", as.character(electionResults$Turnout)))
